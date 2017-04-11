8f24622dd13138e637830a7d11c6b535e80a9bf4
robust <robust>
2017-04-11 10:23:51 +0300

    First Project Upload
    A layout example for the UI and the 3d roulette model.

---
+++ Assets/BallForce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallForce : MonoBehaviour {
+    
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody>().isKinematic = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody>().isKinematic = false;
+
+            this.GetComponent<Rigidbody>().AddForce(0, 0, -150f, ForceMode.VelocityChange);
+        }
+
+    }
+}
