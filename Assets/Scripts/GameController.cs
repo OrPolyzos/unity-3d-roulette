391443a7466f8a8bb56e88f13e8954fe5bf577b0
Ganesh <Ganesh.Sundaramoorthy@cognizant.com>
2017-04-13 16:00:36 +0530

    Folder Structure and Start Game on Jaguar Click

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject Sphere;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void startGame()
+	{
+		Debug.Log ("Game Starts");
+		Sphere.GetComponent<Rigidbody>().isKinematic = false;
+
+		Sphere.GetComponent<Rigidbody>().AddForce(0, 0, -150f, ForceMode.VelocityChange);
+	}
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public float Speed;
+    public float subt;
+	// Use this for initialization
+	void Start () {
+        Speed = Random.Range(300f,350f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        subt = 0.25f;
+        Speed = Speed - subt;
+        if (Speed >= 0)
+        {
+            transform.Rotate(0, 0, Speed * Time.deltaTime);
+        }
+
+
+    }
+}
